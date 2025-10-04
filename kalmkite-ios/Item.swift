a97bb613edc022a0b1268783f32063ac413e7ecf
Antony Baasandorj <antony.baasan@gmail.com>
2025-10-04 12:58:51 -0400

    init

---
+++ kalmkite-ios/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  kalmkite-ios
+//
+//  Created by antair on 10/4/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ kalmkite-iosTests/kalmkite_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  kalmkite_iosTests.swift
+//  kalmkite-iosTests
+//
+//  Created by antair on 10/4/25.
+//
+
+import Testing
+@testable import kalmkite_ios
+
+struct kalmkite_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
